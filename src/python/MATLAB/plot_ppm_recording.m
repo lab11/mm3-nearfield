118c9a24daa778f63bbee0d92eeb669f64def487
m3 <m3@dco.(none)>
2015-11-13 16:17:56 -0500

    adding plotting script

--- src/python/MATLAB/plot_ppm_recording.m
+++ src/python/MATLAB/plot_ppm_recording.m
@@ -1,8 +1,8 @@
 clear all;
 
-raw = gr_read_complex_binary('../s_4_h_8_d_8_c_30_2.dat');
+raw = gr_read_complex_binary('../calibrate.dat');
 data = raw;
 
 figure(1);
-plot((1:length(data)),data);
+plot((1:length(data)),abs(data));
 %plot((1:length(data))./12.5e6,data);
